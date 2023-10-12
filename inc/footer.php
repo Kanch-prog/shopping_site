6e47ac18462da04d3185f46f838f889d45b30a63
Kanch-prog <121807277+Kanch-prog@users.noreply.github.com>
2023-10-12 17:43:36 +0530

    Add files via upload

--- inc/footer.php
+++ inc/footer.php
@@ -1,61 +1,12 @@
-</div>
-   <div class="footer">
-   	  <div class="wrapper">	
-	     <div class="section group">
-				<div class="col_1_of_4 span_1_of_4">
-						<h4>Information</h4>
-						<ul>
-						<li><a href="#">About Us</a></li>
-						<li><a href="#">Customer Service</a></li>
-						<li><a href="#"><span>Advanced Search</span></a></li>
-						<li><a href="#">Orders and Returns</a></li>
-						<li><a href="#"><span>Contact Us</span></a></li>
-						</ul>
-					</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Why buy from us</h4>
-						<ul>
-						<li><a href="about.php">About Us</a></li>
-						<li><a href="faq.php">Customer Service</a></li>
-						<li><a href="#">Privacy Policy</a></li>
-						<li><a href="contact.php"><span>Site Map</span></a></li>
-						<li><a href="preview.php"><span>Search Terms</span></a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>My account</h4>
-						<ul>
-							<li><a href="contact.php">Sign In</a></li>
-							<li><a href="index.php">View Cart</a></li>
-							<li><a href="#">My Wishlist</a></li>
-							<li><a href="#">Track My Order</a></li>
-							<li><a href="faq.php">Help</a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Contact</h4>
-						<ul>
-							<li><span>+94-01713458599</span></li>
-							<li><span>+94-01813458552</span></li>
-						</ul>
-						<div class="social-icons">
-							<h4>Follow Us</h4>
-					   		  <ul>
-							      <li class="facebook"><a href="#" target="_blank"><img src="images/icon-1.png" alt="fb"></a></li>
-							      <li class="twitter"><a href="#" target="_blank"><img src="images/icon-2.png" alt="fb"> </a></li>
-							      <li class="googleplus"><a href="#" target="_blank"><img src="images/icon-3.png" alt="fb"> </a></li>
-							      <li class="contact"><a href="#" target="_blank"><img src="images/icon-4.png" alt="fb"> </a></li>
-							      <div class="clear"></div>
-						     </ul>
-   	 					</div>
-				</div>
-			</div>
-			<div class="copy_right">
-				<p>Practice project &amp; All rights Reseverd </p>
-		   </div>
-     </div>
+ <div class="clear">
+        </div>
     </div>
-	<link href="css/flexslider.css" rel="stylesheet" type="text/css" media="all"/>
-	<script src="js/script.js" type="text/javascript"></script>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; Copyright <a href="http://trainingwithliveproject.com">Training with live project</a>. All Rights Reserved.
+        </p>
+    </div>
 </body>
-</php>
+</html>
--- inc/header.php
+++ inc/header.php
@@ -1,48 +1,77 @@
-<!DOCTYPE php>
+<?php
+  header("Cache-Control: no-cache, must-revalidate");
+  header("Pragma: no-cache"); 
+  header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+  header("Cache-Control: max-age=2592000");
+?>
+<!DOCTYPE html>
+<html>
 <head>
-<title>Store Website</title>
-<meta http-equiv="Content-Type" content="text/php; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
-<link href="css/menu.css" rel="stylesheet" type="text/css" media="all"/>
-<script src="js/jquerymain.js"></script>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Schoolbell&display=swap" rel="stylesheet">
-</head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Admin</title>
+    <link rel="stylesheet" type="text/css" href="css/reset.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/text.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/grid.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/layout.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/nav.css" media="screen" />
+    <link href="css/table/demo_page.css" rel="stylesheet" type="text/css" />
+    <!-- BEGIN: load jquery -->
+    <script src="js/jquery-1.6.4.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="js/jquery-ui/jquery.ui.core.min.js"></script>
+    <script src="js/jquery-ui/jquery.ui.widget.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui/jquery.ui.accordion.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui/jquery.effects.core.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui/jquery.effects.slide.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui/jquery.ui.mouse.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui/jquery.ui.sortable.min.js" type="text/javascript"></script>
+    <script src="js/table/jquery.dataTables.min.js" type="text/javascript"></script>
+    <!-- END: load jquery -->
+    <script type="text/javascript" src="js/table/table.js"></script>
+    <script src="js/setup.js" type="text/javascript"></script>
+	 <script type="text/javascript">
+        $(document).ready(function () {
+            setupLeftMenu();
+		    setSidebarHeight();
+        });
+    </script>
 
+</head>
 <body>
-  <div class="wrap">
-		<div class="header_top">
-			<div class="logo">
-				<a href="index.php"><img src="images/logo.png" alt="" /></a>
-			</div>
-			  <div class="header_top_right">
-			    <div class="search_box">
-				    <form>
-				    	<input type="text" value="Search for Products" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search for Products';}"><input type="submit" value="SEARCH">
-				    </form>
-			    </div>
-			    <div class="shopping_cart">
-					<div class="cart">
-						<a href="#" title="View my shopping cart" rel="nofollow">
-								<span class="cart_title"><img src="images/header_cart.png" alt="login"></span>
-								<span class="no_product"></span>
-							</a>
-						</div>
-			      </div>
-		   <div class="login"><a href="login.php">login</a></div>
-		 <div class="clear"></div>
-	 </div>
-	 <div class="clear"></div>
- </div>
-<div class="menu">
-	<ul id="dc_mega-menu-orange" class="dc_mm-orange">
-	  <li><a href="index.php">Home</a></li>
-	  <li><a href="products.php">Products</a> </li>
-	  <li><a href="topbrands.php">Top Brands</a></li>
-	  <li><a href="cart.php">Cart</a></li>
-	  <li><a href="contact.php">Contact</a> </li>
-	  <div class="clear"></div>
-	</ul>
-</div>
+    <div class="container_12">
+        <div class="grid_12 header-repeat">
+            <div id="branding">
+                <div class="floatleft logo">
+                    <img src="img/livelogo.png" alt="Logo" />
+				</div>
+				<div class="floatleft middle">
+					<h1>Training with live project</h1>
+					<p>www.trainingwithliveproject.com</p>
+				</div>
+                <div class="floatright">
+                    <div class="floatleft">
+                        <img src="img/img-profile.jpg" alt="Profile Pic" /></div>
+                    <div class="floatleft marginleft10">
+                        <ul class="inline-ul floatleft">
+                            <li>Hello Admin</li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="clear">
+                </div>
+            </div>
+        </div>
+        <div class="clear">
+        </div>
+        <div class="grid_12">
+            <ul class="nav main">
+                <li class="ic-dashboard"><a href="index.php"><span>Dashboard</span></a> </li>
+                <li class="ic-form-style"><a href=""><span>User Profile</span></a></li>
+				<li class="ic-typography"><a href="changepassword.php"><span>Change Password</span></a></li>
+				<li class="ic-grid-tables"><a href="inbox.php"><span>Inbox</span></a></li>
+                <li class="ic-charts"><a href=""><span>Visit Website</span></a></li>
+            </ul>
+        </div>
+        <div class="clear">
+        </div>
+    
